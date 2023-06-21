3c6a63c9bc218d4b24409e3e92f66d6e6b1acb43
Adi Arifin <133982719+adidongs1@users.noreply.github.com>
2023-06-22 01:00:03 +0700

    Update inc_koneksi.php

--- dist/inc_koneksi.php
+++ dist/inc_koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$db_host    = "localhost";
-$db_email   = "root";
-$db_pass    = "";
-$db_name    = "db_focusmate";
+$db_host    = {{ secrets.db_host }};
+$db_email   = {{ secrets.db_user }};
+$db_pass    = {{ secrets.db_pass }};
+$db_name    = {{ secrets.db_name }};
 
 $koneksi    = mysqli_connect($db_host, $db_email, $db_pass, $db_name);
 
