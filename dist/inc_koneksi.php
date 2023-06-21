3de93c2ba3216d33465755a381d514ec73cb2214
adidongs1 <adidongs15@gmail.com>
2023-06-22 00:55:15 +0700

    Update database connection parameters

--- dist/inc_koneksi.php
+++ dist/inc_koneksi.php
@@ -1,10 +1,10 @@
 <?php
 $db_host    = "localhost";
-$db_email   = "root";
-$db_pass    = "";
-$db_name    = "db_focusmate";
+$db_user   = "id20918960_adminganteng";
+$db_pass    = "4dmin-g@ntenG";
+$db_name    = "id20918960_db_focusmate";
 
-$koneksi    = mysqli_connect($db_host, $db_email, $db_pass, $db_name);
+$koneksi    = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if(!$koneksi){
    die ("koneksi gagal");
